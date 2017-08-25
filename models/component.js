de910c8babb040ee37795d8cd4477a4260112066
Cassandra Claire <me@ryan.moe>
2017-08-26 00:42:32 +1000

    Implement showAPI

--- models/component.js
+++ models/component.js
@@ -1,16 +1,30 @@
 const Mongoose = require('mongoose');
 const Schema = Mongoose.Schema;
 
+const BASECOMPONENT = 0;
+const COMPOSITECOMPONENT = 1;
+
+const KIND = [ BASECOMPONENT, COMPOSITECOMPONENT ];
+
 const ComponentSchema = new Schema({
     componentId: { type: String, unique: true, required: true },
     name: { type: String, unique: true, required: true },
     price: { type: Number, requred: true },
-    type: { type: Number, required: true }, //is is a base component or is it composite
-    typeString: { type: String, required: true },
+    type: { type: KIND, required: true }, //is is a base component or is it composite
     components: { type: [this] }
 });
 
 const component = Mongoose.model('component', ComponentSchema);
+
+showAPI = {
+    handler: function(request, reply) {
+        reply({
+            'message': "Hi there!"
+        })
+    }
+}
+
 module.exports = {
-    Component: component
+    Component: component,
+    showAPI
 }
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@
 
 //controllers
 const testController = require('../models/testData');
+const componentController = require('../models/component');
 //'./controllers/testController');
 
 
@@ -13,5 +14,8 @@ exports.endpoints = [
     { method: 'POST', path: '/api/v1.0/testData/getOne/', config: testController.getOneTestData },
     { method: 'POST', path: '/api/v1.0/testData/create/', config: testController.create },
     { method: 'PUT', path: '/api/v1.0/testData/update/', config: testController.update },
-    { method: 'DELETE', path: '/api/v1.0/testData/{testId}', config: testController.remove }
+    { method: 'DELETE', path: '/api/v1.0/testData/{testId}', config: testController.remove },
+
+    //components
+    { method: 'GET', path: '/api/v1.1/', config: componentController.showAPI }
 ]
